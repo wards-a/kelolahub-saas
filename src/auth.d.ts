111cb620a3bf55706e1270a20231f2b795cc4c3c
Mawardi Adama <mawardiadama@gmail.com>
2025-07-30 14:53:08 +0700

    error deploy

---
+++ src/auth.d.ts
@@ -0,0 +1,23 @@
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id?: string;
+    } & DefaultSession['user'];
+  }
+
+  interface JWT {
+    id?: string;
+    email?: string;
+    name?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id?: string;
+    email?: string;
+    name?: string;
+  }
+}
